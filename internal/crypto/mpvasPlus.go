7981cb51e18668a4a5afe3194343445deb27f1d8
lingMori <754597673@qq.com>
2025-05-23 20:02:16 +0800

    (add): old files

---
+++ internal/crypto/mpvasPlus.go
@@ -0,0 +1,5 @@
+package mpvas
+
+func InitBasicMpvasModel() {
+
+}
---
+++ internal/crypto/mpvasPlus_test.go
@@ -0,0 +1,17 @@
+package mpvas
+
+func getMPVASPlus() {
+	// MPVAS+ is a variant of MPVAS that uses a different set of parameters
+	// and a different set of operations. It is used for testing purposes only.
+	// The parameters are chosen to be small enough to allow for easy
+	// verification of the results, but large enough to be interesting.
+	// mp.params = &MPVASPlusParams{
+	// 	p:  0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
+	// 	q:  0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
+	// 	g:  0x02,
+	// 	h:  0x03,
+	// 	x:  0x04,
+	// 	y:  0x05,
+	// 	z:  0x06,
+	// }
+}
